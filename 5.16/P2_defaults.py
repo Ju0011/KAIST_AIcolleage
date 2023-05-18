5d10e11ddf634844c206b38e053372afeef0db27
Ju0011 <yhs07240@naver.com>
2023-05-18 17:34:00 +0900

    0516_fin

--- 5.16/P2_defaults.py
+++ 5.16/P2_defaults.py
@@ -4,17 +4,28 @@ import pandas as pd
 
 elice_utils = EliceUtils()
 
-#1번
-df = pd.read_csv('data/defaults.csv')
-print(df.head())
-print(df.columns)
-print(df.describe())
+'''
+csv파일을 읽어온 후, 취업 상태인 사람들과 아닌 사람들의 Bank Balance의 차이 구하기
+'''
 
-#2번
+
+
 
 def main():
-    employed_bank_balance = 0
-    non_employed_bank_balance = 0
+    # 1번
+    df = pd.read_csv('data/defaults.csv')
+
+    # 2번
+    employed_bank_balance = df.loc[df["Employed"] == 1, "Bank Balance"].mean()
+    print(employed_bank_balance)
+
+    # 3번
+    non_employed_bank_balance = df.loc[df["Employed"] == 0, "Bank Balance"].mean()
+    print(non_employed_bank_balance)
+
+    # 4번
+    employed_bank_balance, non_employed_bank_balance = df.loc[df["Employed"] == 1, "Bank Balance"].mean(), df.loc[
+        df["Employed"] == 0, "Bank Balance"].mean()
 
     return (non_employed_bank_balance, employed_bank_balance)
 
