7213aa38b6b1fe483c990bdda72ef1dfb48685bc
Ju0011 <yhs07240@naver.com>
2023-05-18 20:39:11 +0900

    0518_second

---
+++ 5.18/P2.py
@@ -0,0 +1,40 @@
+class Queue:
+    def __init__(self, N=4):
+        self.arr = []
+        self.length = N
+
+
+    def is_full(self):
+        # 큐가 가득 차있는지 확인
+        if self.length == len(self.arr):
+            print("Q가 가득 차있습니다.")
+            return True
+        else:
+            return False
+    def is_empty(self):
+        if len(self.arr) == 0:
+            return True
+        else:
+            return False
+
+
+    def enqueue(self, item):
+        # q안에 item을 넣는다
+        if self.is_full():
+            return
+
+        self.arr.append(item)
+
+    def dequeue(self):
+        #q에서 첫번째 item 제거한다 + 돌려준다
+        if self.is_empty():
+            print("Q가 비어있음")
+            return
+
+        return self.arr.pop(0)
+
+    def top(self):
+        pass
+
+queue = Queue(4)
+print(queue.is_empty())
