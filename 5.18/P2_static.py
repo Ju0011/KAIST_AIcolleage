b832be18c280dd33a0324e2e1e8625e71cc47118
Ju0011 <yhs07240@naver.com>
2023-05-18 21:49:51 +0900

    0518_fin

--- 5.18/P2_static.py
+++ 5.18/P2_static.py
@@ -47,5 +47,60 @@ class Queue:
             return
         return self.arr[-1]
 
-queue = Queue(4)
-print(queue.is_empty())
+    def pop(self,index):
+        if self.is_empty():
+            print("Q가 비어있음")
+            return
+        return self.arr.pop(index)
+
+'''
+import collections
+queue = collections.deque()
+queue.insert(0,'서울')
+queue.insert(0,'인천')
+queue.insert(0,'대구')
+queue.insert(0,'부산')
+
+print(queue)
+
+print(queue.pop())
+print(queue)
+'''
+
+
+
+'''
+1번 왕자부터 n번 왕자까지 원탁에 앉아있는다. 1번 왕자부터 시계방향으로 돌아가며 1부터 시작하여 번호를 외치게 한다.
+한 왕자가 k(특정숫자)를 외치면 그 왕자는 공주를 구하러 가는데서 제외되고 원 밖으로 나오게 된다.
+그리고 다음 왕자부터 다시 1부터 시작하여 번호를 외친다.
+'''
+
+N = 20
+K = 7
+
+def findLast(N,K):
+    '''마지막 왕자를 찾아라'''
+    princeQ = Queue()
+    for i in range(N):
+        princeQ.enqueue(str(i+1))
+
+    #pseudo Code
+    count = 1   #몇을 외치고 있는지
+    point = 0   #
+
+    while(not princeQ.is_empty()):
+        if count % K == 0 and point != 0:
+            last = princeQ.pop(point)
+            count -= K
+            point -= 1
+
+        if point == N:
+            point = point -N
+
+        count += 1
+        point += 1
+
+    print(princeQ.arr)
+    return
+
+findLast(N,K)
