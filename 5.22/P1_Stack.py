d0cb4e3bbefda73d812d330441de54c088f8e455
Ju0011 <yhs07240@naver.com>
2023-05-22 21:04:02 +0900

    0522_9pm

---
+++ 5.22/P1_Stack.py
@@ -0,0 +1,46 @@
+# 입력값 설정
+# N = input()
+N = 2
+sentences = ['I am happy today', 'We want to win the first prize']
+
+# 알고리즘 풀이
+
+# Solution 1 - indexing
+for i in range(N):
+    # input_string = input()
+    input_string = sentences[i]
+
+    # Code
+    stack = []
+    for c in input_string:
+        if c == " ":
+            while len(stack) != 0:
+                print(stack.pop(), end='')
+            print(' ', end='')
+
+        else:
+            stack.append(c)
+
+    while len(stack) != 0:
+        print(stack.pop(), end='')
+
+    print(' ', end='')
+
+    print()
+
+# Solution 2 - Split
+for i in range(N):
+    input_string = sentences[i]
+
+    # Code
+    stack = []
+    words = input_string.split()
+    for word in words:
+        for c in word:
+            stack.append(c)
+
+        while len(stack) != 0:
+            print(stack.pop(), end='')
+        print(' ', end='')
+
+    print()
